1cac8065a8697f8d8849cce4bc8c58940d628c44
Natalie Macias <nramirez787@gmail.com>
2016-05-17 11:51:02 -1000

    setting up initial file structure and adding manifest.json file

---
+++ components/background/background.js
@@ -0,0 +1,95 @@
+'use strict';
+
+//sends history to backend on installation
+chrome.runtime.onInstalled.addListener(function(data){
+  if(data.reason === "install"){
+    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
+    var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
+
+    var urls = {};
+
+    chrome.history.search({
+      'text' : "",
+      'startTime' : oneWeekAgo,
+      'maxResults' : 100000
+    }, function(historyItems){
+
+      historyItems.forEach(function(historyItem){
+        var histUrl = historyItem.url.toLowerCase();
+
+        if(histUrl.indexOf("google") === -1){
+          urls[histUrl] = historyItem;
+
+          chrome.history.getVisits({url : historyItem.url}, function(itemInfo){
+            urls[histUrl].info = itemInfo;
+          });
+        }
+
+      });
+
+      $.ajax({
+        type: "POST",
+        url: "http://127.0.0.1:3000/data",
+        dataType: 'json',
+        data: {
+          urls : JSON.stringify(urls)
+        },
+        success: function(data) {
+          console.log(data);
+        }
+      });
+
+    });
+  }
+});
+
+// //get information before navigation
+// chrome.webNavigation.onBeforeNavigate.addListener(function(data) {
+//   var queryInfo = {
+//     active: true,
+//     currentWindow: true
+//   };
+
+//   chrome.tabs.query(queryInfo, function(tabs) {
+//     if(tabs[0].title){
+//       console.log("URL", tabs[0].title);
+//     }
+//   });
+// });
+
+//sends activity data.
+chrome.webNavigation.onCommitted.addListener(function(data) {
+
+  console.log("activity", data);
+
+  $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:3000/activity",
+    dataType: 'json',
+    data: {
+      committedData: data
+    },
+    success: function(data) {
+
+    }
+  });
+
+});
+
+chrome.webRequest.onCompleted.addListener(function(data){
+
+  console.log("query", data);
+
+  $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:3000/query",
+    dataType: 'json',
+    data: {
+      committedData: data
+    },
+    success: function(data) {
+
+    }
+  });
+
+}, {urls: ["*://www.google.com/search?*"], types: ['xmlhttprequest']});
