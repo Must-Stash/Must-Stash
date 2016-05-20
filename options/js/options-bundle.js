c668179dbdfe9653a17106a9966a9afc43f45747
Owen <owenyangg@gmail.com>
2016-05-20 10:57:02 -1000

    History now sends to QA route

--- options/js/options-bundle.js
+++ options/js/options-bundle.js
@@ -49,41 +49,32 @@
 	var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
 	var oneWeekAgo = new Date().getTime() - microsecondsPerWeek;
 	
-	var urls = {};
-	
 	document.getElementById('sendhistory').addEventListener("click", function () {
+	
 	  chrome.history.search({
 	    'text': "",
 	    'startTime': oneWeekAgo,
 	    'maxResults': 100000
 	  }, function (historyItems) {
 	
-	    historyItems.forEach(function (historyItem) {
-	      var histUrl = historyItem.url.toLowerCase();
-	
-	      if (histUrl.indexOf("google") === -1) {
-	        urls[histUrl] = historyItem;
-	
-	        chrome.history.getVisits({ url: historyItem.url }, function (itemInfo) {
-	          urls[histUrl].info = itemInfo;
-	        });
-	      }
+	    var payload = historyItems.map(function (element) {
+	      return {
+	        activity: element
+	      };
 	    });
 	
 	    $.ajax({
 	      type: "POST",
-	      url: "http://127.0.0.1:3000/data",
+	      url: "http://127.0.0.1:3000/api/qa",
 	      dataType: 'json',
 	      data: {
-	        urls: JSON.stringify(urls)
+	        data: payload
 	      },
 	      success: function success(data) {
 	        console.log(data);
 	      }
 	    });
 	  });
-	
-	  console.log(urls);
 	});
 
 /***/ }
