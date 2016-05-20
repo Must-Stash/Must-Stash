b779a70655682b7ad4800239b6daa6ee56c96c14
Taylor Kennedy <taykenned@gmail.com>
2016-05-20 13:24:04 -1000

    adds history
    history is sent to the database on extension install

--- background/background.js
+++ background/background.js
@@ -3,37 +3,62 @@
 //sends history to backend on installation
 chrome.runtime.onInstalled.addListener(function(data){
   if(data.reason === "install"){
-    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
+    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 90;
     var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
 
     chrome.history.search({
       'text' : "",
       'startTime' : oneWeekAgo,
-      'maxResults' : 100000
+      'maxResults' : 1000000
     }, function(historyItems){
 
-      var payload = historyItems.map(function(element){
+      var chunkLength = 100;
+      var count = 0;
+
+      var payloadArray = historyItems.filter(function(historyItem) {
+        if(historyItem.url.indexOf('google') !== -1 && historyItem.url.indexOf('q=') !== -1) {
+         return false;
+        }
+        return true;
+      }).map(function(element){
         return {
           activity : element
         };
-      });
-
-      $.ajax({
-        type: "POST",
-        url: "http://127.0.0.1:3000/api/qa",
-        dataType: 'json',
-        data: {
-          data : payload
-        },
-        success: function(data) {
-          console.log(data);
+      }).reduce(function(prev, curr) {
+        if(prev[count].length === chunkLength) {
+          prev.push([]);
+          count++;
         }
-      });
+        prev[count].push(curr);
+        return prev;
+      }, [[]]);
+
+      sendRequest(payloadArray, payloadArray.length -1);
 
     });
 
   }
 });
+
+
+function sendRequest(payloadArray, index) {
+
+  if(index < 0) {
+    return;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:3000/api/qa",
+    dataType: 'json',
+    data: {
+      data : payloadArray[index]
+    },
+    success: function(data) {
+      sendRequest(payloadArray, index - 1);
+    }
+  });
+}
 
 //sends activity data.
 chrome.webNavigation.onCommitted.addListener(function(data) {
--- options/js/options-bundle.js
+++ options/js/options-bundle.js
@@ -46,7 +46,7 @@
 
 	'use strict';
 	
-	var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 30;
+	var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 90;
 	var oneWeekAgo = new Date().getTime() - microsecondsPerWeek;
 	
 	document.getElementById('sendhistory').addEventListener("click", function () {
@@ -54,28 +54,52 @@
 	  chrome.history.search({
 	    'text': "",
 	    'startTime': oneWeekAgo,
-	    'maxResults': 100000
+	    'maxResults': 1000000
 	  }, function (historyItems) {
 	
-	    var payload = historyItems.map(function (element) {
+	    var chunkLength = 100;
+	    var count = 0;
+	
+	    var payloadArray = historyItems.filter(function (historyItem) {
+	      if (historyItem.url.indexOf('google') !== -1 && historyItem.url.indexOf('q=') !== -1) {
+	        return false;
+	      }
+	      return true;
+	    }).map(function (element) {
 	      return {
 	        activity: element
 	      };
-	    });
-	
-	    $.ajax({
-	      type: "POST",
-	      url: "http://127.0.0.1:3000/api/qa",
-	      dataType: 'json',
-	      data: {
-	        data: payload
-	      },
-	      success: function success(data) {
-	        console.log(data);
+	    }).reduce(function (prev, curr) {
+	      if (prev[count].length === chunkLength) {
+	        prev.push([]);
+	        count++;
 	      }
-	    });
+	      prev[count].push(curr);
+	      return prev;
+	    }, [[]]);
+	
+	    sendRequest(payloadArray, payloadArray.length - 1);
 	  });
 	});
+	
+	function sendRequest(payloadArray, index) {
+	
+	  if (index < 0) {
+	    return;
+	  }
+	
+	  $.ajax({
+	    type: "POST",
+	    url: "http://127.0.0.1:3000/api/qa",
+	    dataType: 'json',
+	    data: {
+	      data: payloadArray[index]
+	    },
+	    success: function success(data) {
+	      sendRequest(payloadArray, index - 1);
+	    }
+	  });
+	}
 
 /***/ }
 /******/ ]);
